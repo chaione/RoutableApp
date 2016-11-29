11823b9f27ce9a140fce11b266545021cb82eb38
Fabian Buentello <fabian.buentello@chaione.com>
2016-11-29 13:26:58 -0600

    Routable: simple nested

--- RoutableApp/Router.swift
+++ RoutableApp/Router.swift
@@ -13,11 +13,40 @@ struct Router: URLRouter {
         return "https://private-85a46-routable.apiary-mock.com/"
     }
 
-    struct User: Readable, Creatable, Deletable, Updateable {
+    struct User: Readable, Creatable, Updateable {
         var route: String = "users"
+        var urlParams: String!
     }
 
-    struct Post: Readable, Creatable, Updateable {
+    struct Status: Readable, Creatable, Deletable {
+        var route: String = "statuses"
+        var urlParams: String!
+    }
+
+    struct Picture: Readable, Creatable, Deletable, Updateable {
+        var route: String = "pictures"
+        var urlParams: String!
+    }
+
+    struct Comment: Readable, Creatable, Deletable, Updateable {
+        var route: String = "comments"
+        var urlParams: String!
+    }
+
+    struct Post: Readable, Creatable, Deletable, Updateable {
         var route: String = "posts"
+        var urlParams: String!
+    }
+
+    struct Like: Readable, Creatable, Deletable {
+        var route: String  = "likes"
+        var urlParams: String!
+    }
+}
+
+extension Router.User {
+
+    func getStatus(params: String) -> RequestConverterProtocol {
+        return nestedRoute(args: urlParams, child: Router.Status.get(params: params))
     }
 }
