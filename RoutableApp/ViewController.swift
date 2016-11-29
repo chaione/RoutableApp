112b49ff5d747d4c395cee8ad72d061b54eef846
Fabian Buentello <fabian.buentello@chaione.com>
2016-11-29 12:56:05 -0600

    Init commit

---
+++ RoutableApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  RoutableApp
+//
+//  Created by Fabian Buentello on 11/29/16.
+//  Copyright © 2016 ChaiOne. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
+
