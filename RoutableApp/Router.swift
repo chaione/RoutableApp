5bc041e5c38a45765cfa90f4684e80196831c389
Fabian Buentello <fabian.buentello@chaione.com>
2017-02-07 07:57:15 -0600

    final implementation

--- RoutableApp/Router.swift
+++ RoutableApp/Router.swift
@@ -14,20 +14,150 @@ struct Router: URLRouter {
         return "https://private-85a46-routable.apiary-mock.com/"
     }
 
-    struct User: Readable, Creatable, Deletable, Updatable {
+    struct User: Readable, Creatable, Updatable,
+    hasStatuses, hasPictures, hasPosts {
         var route: String = "users"
         var urlParams: String!
     }
 
-    struct Status: Readable, Creatable, Deletable {
+    struct Status: Readable, Creatable, Deletable,
+    hasComments {
         var route: String = "statuses"
+        var urlParams: String!
+    }
+
+    struct Picture: Readable, Creatable, Deletable, Updatable,
+    hasComments {
+        var route: String = "pictures"
+        var urlParams: String!
+    }
+
+    struct Comment: Readable, Creatable, Deletable, Updatable,
+    hasUsers  {
+        var route: String = "comments"
+        var urlParams: String!
+    }
+
+    struct Post: Readable, Creatable, Deletable, Updatable,
+    hasComments {
+        var route: String = "posts"
         var urlParams: String!
     }
 }
 
-extension Router.User {
+// Would probably move this(below code) to a different file called `ChildRoute.swift`
+protocol hasStatuses {}
+extension hasStatuses where Self: Routable {
+    func status(params: String) -> Router.Status {
+        var child = Router.Status(params)
+        child.route = nestedRouteURL(parent: self, child: child)
+        return child
+    }
 
     func getStatus(params: String) -> RequestConverterProtocol {
         return nestedRoute(args: urlParams, child: Router.Status.get(params: params))
+    }
+
+    func createStatus(parameters: Parameters) -> RequestConverterProtocol {
+        return nestedRoute(args: urlParams, child: Router.Status.create(parameters: parameters))
+    }
+
+    func deleteStatus(params: String) -> RequestConverterProtocol {
+        return nestedRoute(args: urlParams, child: Router.Status.delete(params: params))
+    }
+}
+
+protocol hasComments {}
+extension hasComments where Self: Routable {
+    func comment(params: String) -> Router.Comment {
+        var child = Router.Comment(params)
+        child.route = nestedRouteURL(parent: self, child: child)
+        return child
+    }
+
+    func getComment(params: String) -> RequestConverterProtocol {
+        let returnComment = nestedRoute(args: urlParams, child: Router.Comment.get(params: params))
+        return returnComment
+    }
+
+    func createComment(parameters: Parameters) -> RequestConverterProtocol {
+        return nestedRoute(args: urlParams, child: Router.Comment.create(parameters: parameters))
+    }
+
+    func deleteComment(params: String) -> RequestConverterProtocol {
+        return nestedRoute(args: urlParams, child: Router.Comment.delete(params: params))
+    }
+
+    func updateComment(params: String, parameters: Parameters) -> RequestConverterProtocol {
+        return nestedRoute(args: urlParams, child: Router.Comment.update(params: params, parameters: parameters))
+    }
+}
+
+protocol hasPictures {}
+extension hasPictures where Self: Routable {
+    func picture(params: String) -> Router.Picture {
+        var child = Router.Picture(params)
+        child.route = nestedRouteURL(parent: self, child: child)
+        return child
+    }
+
+    func getPicture(params: String) -> RequestConverterProtocol {
+        return nestedRoute(args: urlParams, child: Router.Picture.get(params: params))
+    }
+
+    func createPicture(parameters: Parameters) -> RequestConverterProtocol {
+        return nestedRoute(args: urlParams, child: Router.Picture.create(parameters: parameters))
+    }
+
+    func deletePicture(params: String) -> RequestConverterProtocol {
+        return nestedRoute(args: urlParams, child: Router.Picture.delete(params: params))
+    }
+
+    func updatePicture(params: String, parameters: Parameters) -> RequestConverterProtocol {
+        return nestedRoute(args: urlParams, child: Router.Picture.update(params: params, parameters: parameters))
+    }
+}
+
+protocol hasUsers {}
+extension hasUsers where Self: Routable {
+
+    func user(params: String) -> Router.User {
+        var child = Router.User(params)
+        child.route = nestedRouteURL(parent: self, child: child)
+        return child
+    }
+
+    func getUser(params: String) -> RequestConverterProtocol {
+        return nestedRoute(args: urlParams, child: Router.User.get(params: params))
+    }
+
+    func createUser(parameters: Parameters) -> RequestConverterProtocol {
+        return nestedRoute(args: urlParams, child: Router.User.create(parameters: parameters))
+    }
+
+    func updateUser(params: String, parameters: Parameters) -> RequestConverterProtocol {
+        return nestedRoute(args: urlParams, child: Router.User.update(params: params, parameters: parameters))
+    }
+}
+
+protocol hasPosts {}
+extension hasPosts where Self: Routable {
+
+    func post(params: String) -> Router.Post {
+        var child = Router.Post(params)
+        child.route = nestedRouteURL(parent: self, child: child)
+        return child
+    }
+
+    func getPost(params: String) -> RequestConverterProtocol {
+        return nestedRoute(args: urlParams, child: Router.Post.get(params: params))
+    }
+
+    func createPost(parameters: Parameters) -> RequestConverterProtocol {
+        return nestedRoute(args: urlParams, child: Router.Post.create(parameters: parameters))
+    }
+
+    func updatePost(params: String, parameters: Parameters) -> RequestConverterProtocol {
+        return nestedRoute(args: urlParams, child: Router.Post.update(params: params, parameters: parameters))
     }
 }
--- RoutableApp/ViewController.swift
+++ RoutableApp/ViewController.swift
@@ -14,6 +14,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        _ = Router.Post("54").getComment(params: "43")                                  // posts/54/comments/43
+        _ = Router.User("initFabian").getStatus(params: "2")                            // users/initFabian/statuses/2
+        _ = Router.User("initFabian").picture(params: "2").getComment(params: "3")      // users/initFabian/pictures/2/comments/3
+
+
+        let crazyNested = Router.User("initFabian")
+            .status(params: "3")
+            .comment(params: "32352")
+            .getUser(params: "fakeUser")
+
+        print(crazyNested)
+        // users/initFabian/statuses/3/comments/32352/users/fakeUser
+
+        let extremelyNested = Router.User("initFabian")
+            .picture(params: "3")
+            .comment(params: "507f191e810c19729de860ea")
+            .user(params: "43")
+            .status(params: "firstStatus")
+            .comment(params: "27")
+            .user(params: "63")
+            .post(params: "4d6e5acebcd1b3fac9000002")
+            .comment(params: "82")
+            .user(params: "21")
+            .getStatus(params: "1")
+        
+        print(extremelyNested)
+        
         Alamofire.request(Router.User("2").getStatus(params: "2")).validate().responseJSON { response in
             switch response.result {
             case .success:
